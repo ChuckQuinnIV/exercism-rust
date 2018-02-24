3ef493f1a49b43856fe0a8eff82b3745676d330a
ChuckQuinnIV <cquinniv@gmail.com>
2018-02-24 12:42:34 -0600

    [4] Solved

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!()
+    let mut return_obj = String::with_capacity(60);
+    if n % 3 == 0 { return_obj.push_str("Pling");}
+    if n % 5 == 0 { return_obj.push_str("Plang");}
+    if n % 7 == 0 { return_obj.push_str("Plong");}
+
+    if return_obj.len() == 0 { return_obj.push_str(&n.to_string()); }
+
+    return_obj
 }
